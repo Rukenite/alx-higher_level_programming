8dca1d54f7d29a225bec1607f29914769c57637e
rukenite <rukenite67@gmail.com>
2023-09-25 11:31:21 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    final = 0
+    r = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end="")
-            final += 1
+            print("{}".format(my_list[i]), end="")
+            r += 1
         except IndexError:
             break
     print()
-    return(final)
+    return(r)
